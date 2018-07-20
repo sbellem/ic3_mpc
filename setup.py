5f7dc00cc27ee9b873ae6f0e84ae707becb52853
Sylvain Bellemare <sbellem@gmail.com>
2018-07-20 21:11:44 +0100

    try

--- setup.py
+++ setup.py
@@ -10,5 +10,9 @@ setup(
     name='mpc',
     version='0.0.1',
     py_modules=['number_shoot_out'],
+    install_requires=['viff>=2.0'],
     extras_require={'dev': dev_require},
+    dependency_links=[
+        'git+https://github.com/sbellem/viff.git@setup#egg=2.0',
+    ],
 )
