17d9f2b2e495c7e8bd1c9d3c87a7d4dfe30b9802
Sylvain Bellemare <sbellem@gmail.com>
2018-07-21 01:04:19 +0100

    console script

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
     name='mpc',
     version='0.0.1',
     py_modules=['number_shoot_out'],
+    entry_points = {
+        'console_scripts': ['mpc-numbershootout=number_shoot_out:main'],
+    },
     install_requires=['viff>=2.0'],
     extras_require={'dev': dev_require},
     dependency_links=[
